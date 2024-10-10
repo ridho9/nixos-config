fbdcf64abaa933ffd455bf340f700aeb3ccb1a1d
ridho9 <p.ridho9@gmail.com>
2024-10-10 19:15:39 +0000

    update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -11,19 +14,26 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }@inputs: 
-  let
-    system = "x86_64-linux";
-  in
-  {
-    nixosConfigurations.wsl-nixos = nixpkgs.lib.nixosSystem {
-      system = system;
-      specialArgs = {inherit inputs system;};
-      modules = [
-        nixos-wsl.nixosModules.default
-        ./configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.wsl-nixos = nixpkgs.lib.nixosSystem {
+        system = system;
+        specialArgs = {
+          inherit inputs system;
+        };
+        modules = [
+          inputs.nixos-wsl.nixosModules.default
+          ./configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
     };
-  };
 }
--- home.nix
+++ home.nix
@@ -18,10 +18,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -34,6 +30,9 @@
     # (pkgs.writeShellScriptBin "my-hello" ''
     #   echo "Hello, ${config.home.username}!"
     # '')
+
+    pkgs.nixfmt-rfc-style
+    pkgs.nil
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -85,11 +84,22 @@
       ga = "git add";
       gco = "git add";
     };
+    shellAliases = {
+      cd = "z";
+    };
   };
 
   programs.git = {
     enable = true;
-    userName  = "ridho9";
+    userName = "ridho9";
     userEmail = "p.ridho9@gmail.com";
+  };
+
+  programs.direnv = {
+    enable = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
   };
 }
