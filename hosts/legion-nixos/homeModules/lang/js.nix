5d28ccf3a823dd613c420b3dfa112546b457f6de
ridho9 <p.ridho9@gmail.com>
2025-05-31 12:16:38 +0700

    update

--- hosts/legion-nixos/homeModules/lang/js.nix
+++ hosts/legion-nixos/homeModules/lang/js.nix
@@ -5,9 +5,12 @@
       nodejs
       yarn
       deno
+      pnpm
+      nodePackages."@nestjs/cli"
     ];
 
     programs.bun.enable = true;
 
+    home.sessionVariables.PNPM_HOME = "$HOME/.pnpm";
   };
 }
